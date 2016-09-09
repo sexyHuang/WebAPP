b9a4e4fdaa865571c87d6ce24b876cdd8cf1ce6e
Sexy <sexy.is.353016313@gmail.com>
2016-09-09 17:03:32 +0800

    add WebAPP

---
+++ WebAPP/js/H5.js
@@ -0,0 +1,102 @@
+/**
+ * Created by sexyi on 2016/9/4.
+ */
+/**
+ *   内容组织类
+ * @constructor
+ */
+var H5 = function () {
+    this.id = ('h5_' + Math.random()).replace('.', '_');
+    this.ele = $('<div class="h5" id="' + this.id + '">').hide();
+    this.page = [];
+    $('body').append(this.ele);
+
+    /**
+     * 新增一个页
+     * @param {string} name 页的名称，会加入到ClassName中
+     * @param {string} text 页内的默认文本
+     * @returns {H5} H5对象，可以重复适应H5对象支持的方法
+     */
+    this.addPage = function (name, text) {
+        var page = $('<div class="h5_page section">');
+        if (name != undefined) {
+            page.addClass('h5_page_' + name);
+            page.text(text);
+        }
+        this.ele.append(page);
+        this.page.push(page);
+        if (Object.prototype.toString.call(this.whenAddPage) == '[object Function]')
+            this.whenAddPage();
+        return this;
+    };
+
+    /**
+     * 新增一个组件
+     * @param {string}name 组件的名称，会加入到ClassName中
+     * @param {object}cfg 配置对象
+     * @returns {H5} H5对象，可以重复适应H5对象支持的方法
+     */
+    this.addComponent = function (name, cfg) {
+        var cfg = cfg || {};
+        cfg = $.extend({
+            type: 'base'
+        }, cfg);
+        var component;
+        var page = this.page.slice(-1)[0];
+        switch (cfg.type) {
+            case 'base':
+                component = new H5ComponentBase(name, cfg);
+                break;
+            case 'bar':
+                component = new H5ComponentBar(name, cfg);
+                break;
+            case 'bar_v':
+                component = new H5ComponentBar_v(name, cfg);
+                break;
+            case 'point':
+                component = new H5ComponentPoint(name, cfg);
+                break;
+            case 'polyline':
+                component = new H5ComponentPolyline(name, cfg);
+                break;
+            case 'radar':
+                component = new H5ComponentRadar(name, cfg);
+                break;
+            case 'pie':
+                component = new H5ComponentPie(name, cfg);
+                break;
+            case 'ring':
+                component = new H5ComponentRing(name, cfg);
+                break;
+            default:
+                throw Error('Do not have this type of component');
+        }
+        page.append(component);
+        return this;
+    };
+
+    /**
+     * H5内容加载方法
+     * @returns {H5}
+     */
+    this.loader = function (firstPage) {
+        this.ele.fullpage({
+            onLeave: function (index, nextIndex, direction) {
+                $(this).find('.h5_component').trigger('onLeave');
+            },
+            afterLoad: function (anchorLink, index) {
+                $(this).find('.h5_component').trigger('onLoad');
+            }
+        });
+        this.page[0].find('.h5_component').trigger('onLoad');
+        this.ele.show();
+        if (firstPage) {
+            $.fn.fullpage.moveTo(firstPage);
+        }
+        return this;
+    };
+
+    this.loader = (Object.prototype.toString.call(H5_loading) == '[object Function]') ? H5_loading : this.loader;
+    return this;
+
+};
---
+++ WebAPP/js/H5ComponentBase.js
@@ -0,0 +1,67 @@
+/**
+ * Created by sexyi on 2016/9/4.
+ */
+
+/**
+ * 基本图文组件对象
+ * @param name 组件名称，会加入className中
+ * @param {object}cfg 配置文件
+ * @returns {*|jQuery|HTMLElement} 返回图文组件的jQuery对象
+ * @constructor
+ */
+var H5ComponentBase = function (name, cfg) {
+    var cfg = cfg || {};
+    var id = ('h5_c_' + Math.random()).replace('.', '_');
+    var name = 'h5_component_name_' + name;
+    var cls = ' h5_component_' + cfg.type;
+    var component = $('<div class="h5_component ' + name + cls + '" id="' + id + '">');
+
+    //配置文件的各项可选参数
+    cfg.text && component.text(cfg.text);
+    cfg.width && component.width(cfg.width / 2);
+    cfg.height && component.height(cfg.height / 2);
+    cfg.css && component.css(cfg.css);
+    if(cfg.bg){
+        if(cfg.bg.indexOf('#')==0||cfg.bg.indexOf('rgb')==0){
+            component.css('backgroundColor', cfg.bg);
+        }else {
+            component.css('backgroundImage', 'url(' + cfg.bg + ')');
+        }
+    }
+
+
+    if(Object.prototype.toString.call(cfg.onclick) == '[object Function]'){
+        component.click(cfg.onclick);
+    }
+    if (cfg.center === true) {
+        component.css({
+            marginLeft: -(cfg.width / 4),
+            left: '50%'
+        })
+    }
+    //更多自定义参数
+    if(cfg.img){
+        var img = $('<img style="width: 100%" src="'+cfg.img+'">');
+        component.append(img);
+    }
+
+    //自定义onLoad事件，组件加载时触发
+    component.on('onLoad', function () {
+        setTimeout(function () {
+            component.addClass(cls + '_load').removeClass(cls + '_leave');
+            cfg.animateIn && component.animate(cfg.animateIn);
+        }, cfg.delay || 0);
+
+        return false;
+    });
+
+    //自定义onLeave事件，组件消失时触发
+    component.on('onLeave', function () {
+            component.addClass(cls + '_leave').removeClass(cls + '_load');
+            cfg.animateOut && component.animate(cfg.animateOut);
+
+        return false;
+    });
+
+    return component;
+};
